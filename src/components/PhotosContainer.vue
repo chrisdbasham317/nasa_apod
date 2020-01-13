3eec6dc84382c99456196524987c94fe70706b7d
Chris Basham <chrisdbasham0317@gmail.com>
2020-01-13 11:36:16 -0700

    Add mounted lifecycle to call daily photo api call

---
+++ src/components/PhotosContainer.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <div class="daily-photo">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div class='monthly-photos'>
+
+    </div>
+  </main>
+</template>
+
+<script>
+import { getDailyPhoto } from '../apiCalls';
+export default {
+  name: 'DailyPhoto',
+  props: {
+    msg: String
+  },
+  async mounted() {
+    const photoData = await getDailyPhoto();
+    console.log(photoData);
+  }
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
